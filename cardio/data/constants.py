a594253c987e39924e17ffbabe3fa5d88b7533a2
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2023-02-04 18:21:46 -0500

    Finished reader test

--- cardio/data/constants.py
+++ cardio/data/constants.py
@@ -3,6 +3,14 @@ class DataConstants:
     '''
     All core stuff for data handling
     '''
-    CARDIO_VID = 1452
-    CARDIO_PID = 65535
+    CARDIO_VID = 7375
+    CARDIO_PID = 21074
     CARDIO_SN = 'CARDIOP1'
+
+    @classmethod
+    def get_card_type(self, type: bytes) -> str:
+        return {
+            b'\x01': 'NFC',
+            b'\x02': 'FelICa',
+            b'\x03': 'MiFare Classic'
+        }[type]
--- cardio/tests/cardio.py
+++ cardio/tests/cardio.py
@@ -9,16 +9,45 @@ class CardIOTest:
             print('Unable to find any CardIO Readers!\nPlease make sure to plug in a CardIO device.')
             sys.exit()
 
-        print(f'Found {len(devices)} CardIO device(s)!')
+        print(f'Found {len(devices)} CardIO device(s)!\nNote that ONLY the FIRST CardIO will be used!')
 
         for device in devices:
             if device['serial_number'] != DataConstants.CARDIO_SN:
                 print(f'This CardIO device is not Player 1!n\Refusing to connect!')
                 sys.exit()
+
+            if device['path'].decode('utf-8')[-3:] == 'KBD':
+                continue
             
             device = hid.Device(DataConstants.CARDIO_VID, DataConstants.CARDIO_PID, DataConstants.CARDIO_SN, device['path'])
+            print(f'\nCardIO Information:\nSerial: {device.serial}\nProduct: {device.product}\nManufacturer: {device.manufacturer}')
+
+            return device
+
+    def pollCardIO(self, device: hid.Device):
+        data = device.read(128, 200)
+        if data == b'':
+            return None
+        else:
+            card_type = data[:1]
+
+            card_id = data[1:]
+            if card_id[-4:] == b'\x00\x00\x00\x00':
+                card_id = card_id[:4]
+                card_type = b'\x03'
+            card_id = str(card_id.hex()).upper()
+            
+            print(f'\nYour card information:\nType: {DataConstants.get_card_type(card_type)}\nCardID: {card_id}')
 
 if __name__ == '__main__':
     cardio_test = CardIOTest()
+    device = cardio_test.loadCardIO()
 
-    CardIOTest.loadCardIO(cardio_test)
+    print('\n\nReader was set up properly!')
+    print('Tap a card now to read the ID.')
+    while True:
+        try:
+            cardio_test.pollCardIO(device)
+        except KeyboardInterrupt:
+            print('goodbye!')
+            sys.exit()
